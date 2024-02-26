43f106efa26750d6ea1562a15fbb54555caec934
me-nata <nataoliveirasousa@gmail.com>
2024-02-26 15:27:00 -0300

    fix: excessive line break

--- Backstage/core/template_build.py
+++ Backstage/core/template_build.py
@@ -1,8 +1,14 @@
+import re
+
+
 class TemplateBuilder:
     def render_template(self, template_loaded, template_data: dict):
         rendered_yaml = template_loaded.render(template_data)
-        return rendered_yaml
+        return self.pos_processing(rendered_yaml)
     
+    def pos_processing(self, rendered_yaml):
+        return re.sub(r'\n\s*\n', '\n', rendered_yaml)
+
     def save_template(self, outfile_path, template_obj):
         with open(outfile_path, 'w') as file:
             file.write(template_obj)
